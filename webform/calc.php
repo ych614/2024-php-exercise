e5a761c76edd63a2c1ee50526724fc05fefc2587
Devin Liang <devinliang@gmail.com>
2024-03-19 10:36:07 +0800

    Create calc.php

---
+++ webform/calc.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>計算結果</h1>
+    <p>身高：<?= $_GET["height"] ?></p>
+    <p>體重：<?= $_GET["weight"] ?></p>
+</body>
+</html>
