3dd96706936cbffc97c68f7c2d9b982beaa51781
Devin Liang <devinliang@gmail.com>
2024-03-26 10:44:34 +0800

    Update formele.php

--- webform/formele.php
+++ webform/formele.php
@@ -13,9 +13,23 @@
         <p>姓名：<input type="text" name="chname" id="" placeholder="請輸入中文姓名" required></p>
         <p>密碼：<input type="password" name="passwd" id="" placeholder="請設定密碼"></p>
         <p>身高：<input type="text" name="height" value="" placeholder="請用公分輸入身高"></p>
-        
-        <input type="submit" value="Send">
+
+        <input type="submit" value="Send" name="submit">
     </form>
 
+    <?php
+    # 主機接受並處理
+    if (isset($_REQUEST["submit"])) {
+
+        $name = $_REQUEST["chname"];
+        $pwd = $_REQUEST["passwd"];
+        $h = $_REQUEST["height"];
+
+        echo "姓名:" . $name;
+        echo "密碼:" . $pwd;
+        echo "身高:" . $h;
+    }
+
+    ?>
 </body>
 </html>
