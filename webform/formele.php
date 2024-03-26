fdc3cd0d7b1f10af8503259389d68e7f6d1b7c6b
Devin Liang <devinliang@gmail.com>
2024-03-26 10:23:14 +0800

    Create formele.php

---
+++ webform/formele.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>報名表</h1>
+
+    <form action="" method="post">
+
+        <p>姓名：<input type="text" name="" id=""></p>
+        <p>密碼：<input type="password" name="" id=""></p>
+
+    </form>
+
+</body>
+</html>
